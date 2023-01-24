a6ae34d2eb63848cc5aef99035b521b407ad10ed
p-narasimha-1906 <111100110+p-narasimha-1906@users.noreply.github.com>
2023-01-24 17:14:35 +0530

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,17 @@
+provider "aws"{
+    region="ap-south-1"
+
+}
+
+resource "aws_instance" "server" {
+  ami           = "ami-06984ea821ac0a879"
+  instance_type = "t2.micro"
+  subnet_id="subnet-08ed7a1243a462b83"
+  security_groups=["sg-04d6713736d548093"]
+  
+
+
+  tags = {
+    Name = "terraform-server"
+  }
+}
